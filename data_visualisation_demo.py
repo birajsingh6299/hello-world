058b81572fab2991b3864a0872cb99070f44eb1e
Biraj Singh <birajsingh6299@users.noreply.github.com>
2023-06-11 19:42:59 +0530

    demo code for data visualisation

---
+++ data_visualisation_demo.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Prepare data
+x = [1, 2, 3, 4, 5]
+y = [10, 15, 7, 12, 9]
+
+# Create a line chart
+plt.plot(x, y)
+
+# Add labels and title
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Line Chart')
+
+# Display the chart
+plt.show()
